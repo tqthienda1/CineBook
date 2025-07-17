9f33f7feba79f37ac017a369ccfb2f2bca91592e
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-07-17 11:03:19 +0700

    add prettier config

---
+++ src/Login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginForm from './components/LoginForm';
+
+const Login = () => {
+    return (
+        <div className="background">
+            <div className="login-form">
+                <LoginForm />
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/LoginForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LoginForm = () => {
+    return <h1 className="title">CINEBOOK</h1>;
+};
+
+export default LoginForm;
