9168bc408e948588d4301269807becdfe5237982
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-07-24 10:00:51 +0700

    change background img path

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,10 +1,11 @@
 import LoginForm from '../../components/LoginForm';
 import styles from './Login.module.scss';
+import logoBackground from '../../assets/login_background.jpg';
 
 const Login = () => {
     return (
         <div className={styles.container}>
-            <img className= {styles.background} src="../../login_background.jpg" alt="" />
+            <img className={styles.background} src={logoBackground} alt="" />
             <LoginForm />
         </div>
     );
