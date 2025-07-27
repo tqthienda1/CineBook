243ed6243c75ed2828ae661565d27aec5a6936bf
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-07-28 01:31:32 +0700

    add label content and label color props

--- src/components/PasswordInput/index.js
+++ src/components/PasswordInput/index.js
@@ -3,14 +3,14 @@ import { LuEyeOff } from 'react-icons/lu';
 import styles from './PasswordInput.module.scss';
 import { useState } from 'react';
 
-const PasswordInput = () => {
+const PasswordInput = ({labelContent, labelColor}) => {
     const [hide, setHide] = useState(false);
 
     const handleClick = () => setHide((prev) => !prev);
 
     return (
         <div className={styles.passwordWrap}>
-            <label htmlFor="password">Password</label>
+            <label style={{color: `${labelColor}`}} htmlFor="password">{labelContent}</label>
             <div id = "password" className={styles.passwordInputWrap}>
                 <input type={hide ? 'text' : 'password'} className={styles.passwordInput} />
                 {hide && <MdOutlineRemoveRedEye onClick={handleClick} className={styles.eye} />}
