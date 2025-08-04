bd31b8f5b2d9ec4115cfc08b7bcee5ffb340e9f8
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-08-04 18:30:59 +0700

    add booking page

---
+++ src/pages/Booking/index.js
@@ -0,0 +1,8 @@
+import styles from './Booking.module.scss';
+import MovieDeTail from '../../components/MovieDetail';
+
+const Booking = () => {
+    return <MovieDeTail />;
+};
+
+export default Booking;
