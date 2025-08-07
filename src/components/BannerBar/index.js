db001f2a4a251177735bdd1e05696f0f71b42d95
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-08-07 20:53:35 +0700

    change blur overlay to component

--- src/components/BannerBar/index.js
+++ src/components/BannerBar/index.js
@@ -1,10 +1,11 @@
 import styles from './BannerBar.module.scss';
 import { MdPlayCircle } from 'react-icons/md';
+import BlurOverlay from '../BlurOverlay';
 
 const BannerBar = ({ movieName }) => {
     return (
         <div className={styles.bannerBar}>
-            <div className={styles.overlay}></div>
+            <BlurOverlay />
             <h1 className={styles.movieName}>{movieName}</h1>
             <div className={styles.buttons}>
                 <button className={styles.buyTicket}>BUY TICKET NOW</button>
