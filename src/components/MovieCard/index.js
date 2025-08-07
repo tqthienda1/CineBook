0a52f3c1a5464acc07e3c6a4e4f9ccc2f4fccfd3
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-08-07 21:02:42 +0700

    link to booking page

--- src/components/MovieCard/index.js
+++ src/components/MovieCard/index.js
@@ -1,14 +1,19 @@
 import styles from './MovieCard.module.scss';
 import { MdPlayCircle } from 'react-icons/md';
 import Button from '../Button';
+import { Link } from 'react-router-dom';
 
 const MovieCard = ({ movieName, imgSource }) => {
     return (
         <div className={styles.movieCard}>
-            <img className={styles.poster} src={imgSource} alt="" />
-            <h2 className={styles.movieName}>{movieName}</h2>
+            <Link to="/booking">
+                <img className={styles.poster} src={imgSource} alt="" />
+                <h2 className={styles.movieName}>{movieName}</h2>
+            </Link>
             <div className={styles.buttons}>
-                <Button>Buy tickets</Button>
+                <Link to="/booking">
+                    <Button>Buy tickets</Button>
+                </Link>
                 <h2 className={styles.trailer}>
                     <MdPlayCircle className={styles.playButton} />
                     Trailer
