6bb2f00f07b8f64d23044a83a3246cc72aaf33e0
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-08-08 17:57:36 +0700

    fix center movies container

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,26 +1,26 @@
-import MovieCard from "../../components/MovieCard";
+import MovieCard from '../../components/MovieCard';
 import poster1 from '../../assets/f1_poster.jpg';
-import styles from './Movies.module.scss'
+import styles from './Movies.module.scss';
 
 const Movies = () => {
-    return (
-        <div className={styles.background}>
-            <h1>NOW SHOWING</h1>
-            <div className={styles.movieContainer}>
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-                <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-            </div>
-        </div>
+  return (
+    <div className={styles.background}>
+      <h1>NOW SHOWING</h1>
+      <div className={styles.movieContainer}>
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+      </div>
+    </div>
   );
-}
+};
 
 export default Movies;
