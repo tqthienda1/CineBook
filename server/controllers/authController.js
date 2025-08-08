1dd261b0ac4234080a0c4e2c1a37f3255888a77a
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-08-08 13:04:22 +0700

    test fetch register

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,19 +1,24 @@
 import User from '../models/User.js';
 
-
 export const registerUser = async (req, res) => {
-  const { username, password } = req.body;
+  try{  const { username, password } = req.body;
 
-  if (!username || !password) {
-    return res.status(400).json({ message: 'Thiếu username hoặc password' });
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Thiếu username hoặc password' });
+    }
+
+    // const existingUser = await User.findByUsername(username);
+    // if (existingUser) {
+    //   return res.status(409).json({ message: 'Tên người dùng đã tồn tại' });
+    // }
+
+    // const newUser = await User.create({ username, password });
+
+    console.log("Email: ", email)
+
+    return res.status(201).json({ message: 'Đăng ký thành công', user: email });
+  } catch {
+    console.error("Lỗi khi đăng ký:", err);
+    res.status(500).json({ message: 'Server error' });
   }
-
-  const existingUser = await User.findByUsername(username);
-  if (existingUser) {
-    return res.status(409).json({ message: 'Tên người dùng đã tồn tại' });
-  }
-
-  const newUser = await User.create({ username, password });
-
-  return res.status(201).json({ message: 'Đăng ký thành công', user: newUser });
-};
+  };
--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -12,7 +12,7 @@ const RegisterForm = () => {
 
   const handleRegister = async () => {
     try {
-      const res = await fetch('http://localhost:5000/register', {
+      const res = await fetch('http://localhost:5003/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email }),
