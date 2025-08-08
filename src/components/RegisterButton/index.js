8a5f41845155c15a7a248bd1ca98c4f2713a8c07
Tran Quoc Thien <tqthien23@clc.fitus.edu.vn>
2025-08-08 12:30:13 +0700

    add fetch email to register api

--- src/components/RegisterButton/index.js
+++ src/components/RegisterButton/index.js
@@ -1,8 +1,12 @@
 import styles from './RegisterButton.module.scss';
 import clsx from 'clsx';
 
-const RegisterButton = ({ className }) => {
-    return <button className={clsx(styles.registerButton, className)}>Register</button>;
+const RegisterButton = ({ className, onClick }) => {
+  return (
+    <button onClick={onClick} className={clsx(styles.registerButton, className)}>
+      Register
+    </button>
+  );
 };
 
 export default RegisterButton;
--- src/components/RegisterForm/index.js
+++ src/components/RegisterForm/index.js
@@ -5,26 +5,43 @@ import SeparateLine from '../SeparateLine';
 import styles from './RegisterForm.module.scss';
 import logo from '../../assets/logo.png';
 import RegisterButton from '../RegisterButton';
+import { useState } from 'react';
 
 const RegisterForm = () => {
-    return (
-        <div className={styles.registerForm}>
-            <img className={styles.logo} src={logo} alt="logo"></img>
-            <div className={styles.description}>
-                <h1 className={styles.welcome}>Welcome to CineBook</h1>
-                <p className={styles.subtitle}>Enter your email and password to create your account.</p>
-            </div>
-            <EmailInput />
-            <PasswordInput labelContent={'Password'}/>
-            <PasswordInput labelContent={'Re- enter the password'}/>
-            
-            <RegisterButton />
-            <SeparateLine text="Or Register With" />
+  const [email, setEmail] = useState('');
 
-            <SocialLogin />
-        </div>
-    );
+  const handleRegister = async () => {
+    try {
+      const res = await fetch('http://localhost:5000/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email }),
+      });
+
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <div className={styles.registerForm}>
+      <img className={styles.logo} src={logo} alt="logo"></img>
+      <div className={styles.description}>
+        <h1 className={styles.welcome}>Welcome to CineBook</h1>
+        <p className={styles.subtitle}>Enter your email and password to create your account.</p>
+      </div>
+      <EmailInput value={email} onChange={setEmail} />
+      <PasswordInput labelContent={'Password'} />
+      <PasswordInput labelContent={'Re- enter the password'} />
+
+      <RegisterButton onClick={handleRegister} />
+      <SeparateLine text="Or Register With" />
+
+      <SocialLogin />
+    </div>
+  );
 };
 
 export default RegisterForm;
-
