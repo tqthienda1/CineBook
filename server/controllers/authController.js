a5b5970171dd9ff19ba49dfde23f37f2194549fc
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-08-10 23:06:29 +0700

    feat(login): correct password hashing

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,25 +1,62 @@
-  import User from '../models/User.js';
+import User from '../models/User.js';
 
-  export const registerUser = async (req, res) => {
-    try{  const { email, password } = req.body;
+export const registerUser = async (req, res) => {
+  try{  
+    const { email, password } = req.body;
 
-      if (!email || !password) {
-        return res.status(400).json({ message: 'Thiếu email hoặc password' });
-      }
-
-      const existingUser = await User.checkEmailExists(email);
-      if (existingUser) {
-        return res.status(409).json({ message: 'Email đã được đăng kí' });
-      }
-
-      const newUser = await User.createUser( email, password );x
-
-      // console.log("Username: ", email)
-      // console.log("Password: ", password)
-
-      return res.status(201).json({ message: 'Đăng ký thành công', email: email });
-    } catch (err) {
-      console.error("Lỗi khi đăng ký:", err);
-      res.status(500).json({ message: 'Server error' });
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Thiếu email hoặc password' });
     }
-  };
+
+    const existingUser = await User.checkEmailExists(email);
+    if (existingUser) {
+      return res.status(409).json({ message: 'Email đã được đăng kí' });
+    }
+
+    const newUser = await User.createUser( email, password );
+
+    // console.log("Username: ", email)
+    // console.log("Password: ", password)
+
+    return res.status(201).json({ message: 'Đăng ký thành công', email: email });
+  } catch (err) {
+    console.error("Lỗi khi đăng ký:", err);
+    res.status(500).json({ message: 'Server error' });
+  }
+};
+
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: 'Thiếu email hoặc password' });
+    }
+
+    const existingUser = await User.checkEmailExists(email);
+    if (!existingUser) {
+      return res.status(409).json({ message: 'Email không tồn tại' });
+    }
+
+    const hashPassword = User.getUserByEmail(email)
+    const isPasswordValid = await User.comparePassword(password, hashPassword);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Sai mật khẩu' });
+    }
+
+    const token = jwt.sign(
+      { id: existingUser.id, email: existingUser.email },
+      process.env.JWT_SECRET,
+      { expiresIn: '1h' }
+    );
+
+    return res.status(200).json({
+      message: 'Đăng nhập thành công',
+      token
+    });
+  } catch (err) {
+    console.error("Lỗi khi đăng nhập:", err);
+    res.status(500).json({ message: 'Server error' });
+  }
+}
+
