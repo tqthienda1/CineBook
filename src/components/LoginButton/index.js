90ba46f6a9605a849a22e77c40a745ed8592c957
Tran Quoc Thien <aloxezthien2005@gmail.com>
2025-08-14 17:46:11 +0700

    add send login request from client

--- src/components/LoginButton/index.js
+++ src/components/LoginButton/index.js
@@ -1,8 +1,12 @@
 import styles from './LoginButton.module.scss';
 import clsx from 'clsx';
 
-const LoginButton = ({ className }) => {
-    return <button className={clsx(styles.loginButton, className)}>Log In</button>;
+const LoginButton = ({ onClick, className }) => {
+    return (
+        <button onClick={onClick} className={clsx(styles.loginButton, className)}>
+            Log In
+        </button>
+    );
 };
 
 export default LoginButton;
