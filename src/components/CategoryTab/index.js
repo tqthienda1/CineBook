90670f34d7c2b06b51d24aeaf3769f7780134c23
Thinh <thinh@gmail.com>
2025-08-20 00:46:07 +0700

    update homepage

--- src/components/CategoryTab/index.js
+++ src/components/CategoryTab/index.js
@@ -3,25 +3,18 @@ import VerticalLine from '../VerticalLine';
 import { useState } from 'react';
 import Tab from '../Tab';
 
-const CategoryTab = () => {
-    const [active, setActive] = useState('showing');
-
-    const handleClick = (tab) => {
-        setActive(tab);
-    };
-
-    return (
-        <div className={styles.categoryTab}>
-            <Tab turnActive={active === 'showing'} onClick={() => handleClick('showing')} className={styles.tab}>
-                Now Showing
-            </Tab>
-            <VerticalLine width="2px" color="white" />
-
-            <Tab turnActive={active === 'coming'} onClick={() => handleClick('coming')} className={styles.tab}>
-                Coming Soon
-            </Tab>
-        </div>
-    );
+const CategoryTab = ({ active, onChange }) => {
+  return (
+    <div className={styles.categoryTab}>
+      <Tab turnActive={active === 'showing'} onClick={() => onChange('showing')} className={styles.tab}>
+        Now Showing
+      </Tab>
+      <VerticalLine width="2px" color="white" />
+      <Tab turnActive={active === 'coming'} onClick={() => onChange('coming')} className={styles.tab}>
+        Coming Soon
+      </Tab>
+    </div>
+  );
 };
 
 export default CategoryTab;
