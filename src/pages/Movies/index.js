a1aa7aea7cc759335773fc9e683a0319e6b7c3c7
Thinh <thinh@gmail.com>
2025-08-19 21:25:27 +0700

    solve respone delete movie

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -1,23 +1,42 @@
 import MovieCard from '../../components/MovieCard';
 import poster1 from '../../assets/f1_poster.jpg';
 import styles from './Movies.module.scss';
+import { useState, useEffect } from 'react';
 
 const Movies = () => {
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch('http://localhost:5003/movies', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error('Lỗi khi lấy danh sách phim');
+        }
+        console.log(res);
+
+        const data = await res.json();
+        console.log(data);
+        setMovies(data);
+      } catch (err) {
+        console.error('Fetch movies error:', err);
+      }
+    };
+
+    fetchMovies();
+  }, []);
   return (
     <div className={styles.background}>
       <h1>NOW SHOWING</h1>
       <div className={styles.movieContainer}>
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
-        <MovieCard movieName={'F1 The Movie'} imgSource={poster1} />
+        {movies.map((movie) => (
+          <MovieCard movieName={movie.name} imgSource={movie.posterURL} />
+        ))}
       </div>
     </div>
   );
