4a67fefee71601d424b5f394976870653f1991d7
Thinh <thinh@gmail.com>
2025-08-20 12:21:29 +0700

    solve button to booking page

--- src/components/BannerBar/index.js
+++ src/components/BannerBar/index.js
@@ -1,21 +1,24 @@
 import styles from './BannerBar.module.scss';
 import { MdPlayCircle } from 'react-icons/md';
 import BlurOverlay from '../BlurOverlay';
+import { Link } from 'react-router-dom';
 
 const BannerBar = ({ movieName }) => {
-    return (
-        <div className={styles.bannerBar}>
-            <BlurOverlay />
-            <h1 className={styles.movieName}>{movieName}</h1>
-            <div className={styles.buttons}>
-                <button className={styles.buyTicket}>BUY TICKET NOW</button>
-                <button className={styles.trailer}>
-                    <MdPlayCircle className={styles.playIcon} />
-                    TRAILER
-                </button>
-            </div>
-        </div>
-    );
+  return (
+    <div className={styles.bannerBar}>
+      <BlurOverlay />
+      <h1 className={styles.movieName}>{movieName}</h1>
+      <div className={styles.buttons}>
+        <Link to={'/booking'}>
+          <button className={styles.buyTicket}>BUY TICKET NOW</button>
+        </Link>
+        <button className={styles.trailer}>
+          <MdPlayCircle className={styles.playIcon} />
+          TRAILER
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default BannerBar;
