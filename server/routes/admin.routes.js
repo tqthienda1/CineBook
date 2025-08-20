f697f710e85963d46aa6ce9e03bb78df7ed54772
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-08-20 12:21:50 +0700

    feature(admin): add getAllUser

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { getProfile } from '../controllers/userController.js';
 import { verifyToken } from '../middlewares/authMiddleware.js';
 import { checkAdminRole } from '../middlewares/roleMiddleware.js';
 
-import { getMovies, addMovie, editMovie, deleteMovie } from '../controllers/adminController.js';
+import { getMovies, addMovie, editMovie, deleteMovie, getAllUsers } from '../controllers/adminController.js';
+
 
 
 const router = express.Router();
@@ -12,5 +12,8 @@ router.get('/movies', verifyToken, checkAdminRole, getMovies);
 router.post('/movies', verifyToken, checkAdminRole, addMovie);
 router.put('/movies/:movieID', verifyToken, checkAdminRole, editMovie);
 router.delete('/movies/:movieID', verifyToken, checkAdminRole, deleteMovie);
+
+// Các route cho quản lý người dùng
+router.get('/users', verifyToken, checkAdminRole, getAllUsers);
 
 export default router;
