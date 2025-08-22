bf180b92eb59fbb35f622e2634bf6c21b6c77df2
Tran Quoc Thien <aloxezthien2005@gmail.com>
2025-08-22 17:55:29 +0700

    change variable to match with variable from backend

--- src/components/MovieDetail/index.js
+++ src/components/MovieDetail/index.js
@@ -9,93 +9,93 @@ import clsx from 'clsx';
 import SeparateLine from '../SeparateLine';
 
 const MovieDeTail = ({
-    poster,
-    title,
-    category,
-    language,
-    duration,
-    imdb,
-    ageLimit,
-    releasedDate,
-    description,
-    director,
-    writer,
-    actor,
+  poster,
+  title,
+  category,
+  language,
+  duration,
+  imdb,
+  ageLimit,
+  releaseDate,
+  description,
+  director,
+  writer,
+  actor,
 }) => {
-    //Chuyển định dạng ngày
-    const [day, month, year] = releasedDate.split('/');
-    const dateObj = new Date(year, month - 1, day);
+  //Chuyển định dạng ngày
+  const [day, month, year] = releaseDate.split('/');
+  const dateObj = new Date(year, month - 1, day);
 
-    return (
-        <div className={clsx(styles.movieDetail, styles.row)}>
-            <img className={styles.poster} src={poster} alt="" />
+  return (
+    <div className={clsx(styles.movieDetail, styles.row)}>
+      <img className={styles.poster} src={poster} alt="" />
 
-            <div className={clsx(styles.col, styles.info)}>
-                <h1>{title}</h1>
+      <div className={clsx(styles.col, styles.info)}>
+        <h1>{title}</h1>
 
-                <div className={clsx(styles.row, styles.subinfo1)}>
-                    <ul className={styles.row}>
-                        <LuTags />
-                        {category.map((item, index) => (
-                            <li key={index}>{item + (index !== category.length - 1 ? ', ' : '')}</li>
-                        ))}
-                    </ul>
+        <div className={clsx(styles.row, styles.subinfo1)}>
+          <ul className={styles.row}>
+            <LuTags />
+            {category.map((item, index) => (
+              <li key={index}>{item + (index !== category.length - 1 ? ', ' : '')}</li>
+            ))}
+          </ul>
 
-                    <div>
-                        <MdLanguage />
-                        {language}
-                    </div>
+          <div>
+            <MdLanguage />
+            {language}
+          </div>
 
-                    <div>
-                        <BiTime />
-                        {duration}
-                    </div>
-                </div>
-
-                <div className={clsx(styles.row, styles.subinfo2)}>
-                    <div>
-                        <LiaImdb className={styles.imdb} />
-                        {imdb}/10
-                    </div>
-
-                    <div className={styles.ageLimit}>PG-{ageLimit}</div>
-
-                    <div>
-                        <MdDateRange className={styles.dateIcon} />
-                        {format(dateObj, 'MMM d, yyyy')}
-                    </div>
-                </div>
-
-                <p className={styles.description}>{description}</p>
-            </div>
-
-            <div className={clsx(styles.col, styles.creator)}>
-                <SeparateLine />
-                <h2>Directors</h2>
-                <ul>
-                    {director.map((item, index) => (
-                        <li key={index}>{item + (index !== director.length - 1 ? ',' : '')}</li>
-                    ))}
-                </ul>
-
-                <SeparateLine />
-                <h2>Writers</h2>
-                <ul>
-                    {writer.map((item, index) => (
-                        <li key={index}>{item + (index !== writer.length - 1 ? ',' : '')}</li>
-                    ))}
-                </ul>
-
-                <SeparateLine />
-                <h2>Actor</h2>
-                <ul>
-                    {actor.map((item, index) => (
-                        <li key={index}>{item + (index !== actor.length - 1 ? ',' : '')}</li>
-                    ))}
-                </ul>
-            </div>
+          <div>
+            <BiTime />
+            {duration}
+          </div>
         </div>
-    );
+
+        <div className={clsx(styles.row, styles.subinfo2)}>
+          <div>
+            <LiaImdb className={styles.imdb} />
+            {imdb}/10
+          </div>
+
+          <div className={styles.ageLimit}>PG-{ageLimit}</div>
+
+          <div>
+            <MdDateRange className={styles.dateIcon} />
+            {format(dateObj, 'MMM d, yyyy')}
+          </div>
+        </div>
+
+        <p className={styles.description}>{description}</p>
+      </div>
+
+      <div className={clsx(styles.col, styles.creator)}>
+        <SeparateLine />
+        <h2>Directors</h2>
+        <ul>
+          {director.map((item, index) => (
+            <li key={index}>{item + (index !== director.length - 1 ? ',' : '')}</li>
+          ))}
+        </ul>
+
+        <SeparateLine />
+        <h2>Writers</h2>
+        <ul>
+          {writer.map((item, index) => (
+            <li key={index}>{item + (index !== writer.length - 1 ? ',' : '')}</li>
+          ))}
+        </ul>
+
+        <SeparateLine />
+        <h2>Actor</h2>
+        <ul>
+          {actor.map((item, index) => (
+            <li key={index}>{item + (index !== actor.length - 1 ? ',' : '')}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default MovieDeTail;
--- src/pages/Booking/index.js
+++ src/pages/Booking/index.js
@@ -7,24 +7,24 @@ import SeparateLine from '../../components/SeparateLine';
 import DateSlider from '../../components/DateSlider';
 import Cinema from '../../components/Cinema';
 import Seat from '../../components/Seat';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-const movieInfo = {
-  poster: poster,
-  title: 'F1 The Movie',
-  category: ['Action', 'Drama'],
-  language: 'US',
-  duration: 156,
-  imdb: 7.9,
-  ageLimit: 13,
-  releasedDate: '26/06/2025',
-  description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-  director: ['Joseph Kosinski'],
-  writer: ['duc thinh', 'baolong'],
-  actor: ['quoc thien'],
-};
+// const movieInfo = {
+//   poster: poster,
+//   title: 'F1 The Movie',
+//   category: ['Action', 'Drama'],
+//   language: 'US',
+//   duration: 156,
+//   imdb: 7.9,
+//   ageLimit: 13,
+//   releasedDate: '26/06/2025',
+//   description:
+//     'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+//   director: ['Joseph Kosinski'],
+//   writer: ['duc thinh', 'baolong'],
+//   actor: ['quoc thien'],
+// };
 
 const cinemaInfo = [
   { cinemaName: 'CineBook Quốc Thanh', address: '271 Nguyễn Trãi, Phường Nguyễn Cư Trinh, Thành Phố Hồ Chí Minh' },
@@ -33,12 +33,12 @@ const cinemaInfo = [
 
 const Booking = () => {
   const { movieID } = useParams();
-  const [movieInfo, setMovieInfo] = useState();
+  const [movieInfo, setMovieInfo] = useState(null);
 
   useEffect(() => {
     const fetchMovieInfo = async () => {
       try {
-        const res = await fetch(`http://localhost:5003/user/movie/${movieID}`, {
+        const res = await fetch(`http://localhost:5003/user/movies/${movieID}`, {
           method: 'GET',
           headers: {
             'Content-Type': 'application/json',
@@ -57,7 +57,7 @@ const Booking = () => {
         console.error('Fetch movie error:', err);
       }
     };
-
+    console.log(movieInfo);
     fetchMovieInfo();
   }, [movieID]);
 
@@ -82,50 +82,52 @@ const Booking = () => {
     setSelectCity(city);
   };
 
-  return (
-    <div className={styles.container}>
-      <div className={styles.thumbnailWrapper}>
-        <img className={styles.thumbnail} src={thumbnail} alt="" />
-        <FaCirclePlay className={styles.playButton} />
+  if (movieInfo) {
+    return (
+      <div className={styles.container}>
+        <div className={styles.thumbnailWrapper}>
+          <img className={styles.thumbnail} src={thumbnail} alt="" />
+          <FaCirclePlay className={styles.playButton} />
+        </div>
+        <MovieDeTail {...movieInfo} />
+
+        <div className={styles.margin}></div>
+        <SeparateLine text="SHOWTIME" lineColor={'#fb2b2b'} className={styles.separateLine} />
+        <div className={styles.margin}></div>
+        <DateSlider
+          pickingDate={pickingDate}
+          onSetDate={handleSetDate}
+          pickingDay={pickingDay}
+          onSelectCity={handleSelectCity}
+        />
+        <div className={styles.margin}></div>
+
+        {selectCity && (
+          <>
+            <SeparateLine text="CINEMA LIST" lineColor={'#fb2b2b'} className={styles.separateLine} />
+            <Cinema
+              cinemaList={cinemaInfo}
+              onSelectShowtime={handleSelectShowtime}
+              selectSectionShowtime={selectSectionShowtime}
+              selectShowtime={selectShowtime}
+            />
+          </>
+        )}
+
+        {selectShowtime && (
+          <>
+            <SeparateLine text="SEAT" lineColor={'#fb2b2b'} className={styles.separateLine} />
+            <Seat
+              movieName={'F1 The Movie'}
+              date={pickingDate.toLocaleDateString('vi-VN')}
+              cinemaName={'CineBook Quốc Thanh'}
+              time={selectTime}
+            />
+          </>
+        )}
       </div>
-      <MovieDeTail {...movieInfo} />
-
-      <div className={styles.margin}></div>
-      <SeparateLine text="SHOWTIME" lineColor={'#fb2b2b'} className={styles.separateLine} />
-      <div className={styles.margin}></div>
-      <DateSlider
-        pickingDate={pickingDate}
-        onSetDate={handleSetDate}
-        pickingDay={pickingDay}
-        onSelectCity={handleSelectCity}
-      />
-      <div className={styles.margin}></div>
-
-      {selectCity && (
-        <>
-          <SeparateLine text="CINEMA LIST" lineColor={'#fb2b2b'} className={styles.separateLine} />
-          <Cinema
-            cinemaList={cinemaInfo}
-            onSelectShowtime={handleSelectShowtime}
-            selectSectionShowtime={selectSectionShowtime}
-            selectShowtime={selectShowtime}
-          />
-        </>
-      )}
-
-      {selectShowtime && (
-        <>
-          <SeparateLine text="SEAT" lineColor={'#fb2b2b'} className={styles.separateLine} />
-          <Seat
-            movieName={'F1 The Movie'}
-            date={pickingDate.toLocaleDateString('vi-VN')}
-            cinemaName={'CineBook Quốc Thanh'}
-            time={selectTime}
-          />
-        </>
-      )}
-    </div>
-  );
+    );
+  }
 };
 
 export default Booking;
