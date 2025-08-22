2b5a280dd8dd4ac47146cf19e3e8fc713e993bf9
Ngô Bảo Long <nblong23@.clc.fitus.edu.vn>
2025-08-22 18:01:04 +0700

    fix: format releaseDay

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -4,6 +4,7 @@ import { getMovies } from '../controllers/adminController.js';
 import { verifyToken } from '../middlewares/authMiddleware.js';
 
 const router = express.Router();
+
 // manage user profile
 router.get('/profile', verifyToken, getUserById);
 router.put('/profile', verifyToken, updateUser);
