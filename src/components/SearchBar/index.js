38b98f42b171440503a528b23bb84dc87b690a29
Tran Quoc Thien <aloxezthien2005@gmail.com>
2025-08-23 17:42:47 +0700

    add search movie by name on client side

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,18 +1,33 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.scss';
 import { FaMagnifyingGlass } from 'react-icons/fa6';
 
-const SearchBar = () => {
-    return (
-        <div className={styles.searchBarWrapper}>
-            <div className={styles.searchInputWrapper}>
-                <label className={styles.searchLabel} htmlFor="searchBar">
-                    SEARCH
-                </label>
-                <input className={styles.searchInput} id="searchBar" placeholder="Enter movie name" type="text" />
-                <FaMagnifyingGlass className={styles.magnifyingGlass} />
-            </div>
-        </div>
-    );
+const SearchBar = ({ handleSearchSubmit }) => {
+  const [value, setValue] = useState();
+
+  return (
+    <div className={styles.searchBarWrapper}>
+      <div className={styles.searchInputWrapper}>
+        <label className={styles.searchLabel} htmlFor="searchBar">
+          SEARCH
+        </label>
+        <input
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              handleSearchSubmit(value);
+            }
+          }}
+          className={styles.searchInput}
+          id="searchBar"
+          placeholder="Enter movie name"
+          type="text"
+        />
+        <FaMagnifyingGlass className={styles.magnifyingGlass} onClick={() => handleSearchSubmit(value)} />
+      </div>
+    </div>
+  );
 };
 
 export default SearchBar;
---
+++ src/pages/Search/index.js
@@ -0,0 +1,50 @@
+import MovieCard from '../../components/MovieCard';
+import styles from './Search.module.scss';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+const Search = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log('name', searchParams.get('name'));
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(`http://localhost:5003/user/movies/search/?name=${searchParams.name}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+
+        if (!res.ok) {
+          throw new Error('Lỗi khi lấy danh sách phim');
+        }
+        console.log(res);
+
+        const data = await res.json();
+        setMovies(data);
+      } catch (err) {
+        console.error('Fetch movies error:', err);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+  return (
+    <div className={styles.background}>
+      <h1>SEARCH RESULTS</h1>
+      <div className={styles.movieContainer}>
+        {movies.map((movie) => (
+          <>
+            <MovieCard movieName={movie.name} imgSource={`/assets/${movie.posterURL}`} movieID={movie.movieID} />
+            {console.log(movie.posterURL)}
+          </>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
